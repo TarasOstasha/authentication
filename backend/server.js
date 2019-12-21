fa75e73e5625b6c6037772716095df1b0004a7a7
TarasOstasha <tonyjoss1990@gmail.com>
2019-12-21 10:57:29 -0500

    delete gitignores

--- backend/server.js
+++ backend/server.js
@@ -3,36 +3,53 @@ var cors = require('cors');
 const app = express()
 let bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+const router = express.Router();
 
 
-const posts = [
-    { message: 'first post' },
-    { message: 'second post' }
-]
+
+// const posts = [
+//     { message: 'first post' },
+//     { message: 'second post 34' }
+// ]
 var UserData = require('./models/User.js');
 app.use(cors())
 app.use(bodyParser.json())
-
-app.get('/posts', cors(), (req, res) => {
-    res.send(posts);
+app.use((req,res,next)=>{
+    console.log('request URL!!!!!!!!!',req.url)
+    next();
 })
+
+// app.get('/posts', cors(), (req, res) => {
+//     res.send(posts);
+// })
 
 app.post('/register', cors(), async (req, res) => {
     try {
         const userData = req.body;
         let user = new UserData(userData);
-        console.log('new user-', user)
+        //console.log('new user-', user)
         user.save((err, result) => {
             if (err)
                 console.log('saving user error')
             //res.send(200);
             res.send(result)
         })
-        //console.log(userData.email, userData.password)
     } catch (error) {
         console.log('error', error)
     }
+})
 
+app.post('/login', cors(), async(req, res)=>{
+    console.log('login');
+    try {
+        const userData = req.body.email;
+        console.log('userData', userData);
+        const user = await UserData.findOne({ userData })  //request to database
+        console.log('userLogin', user);
+        res.send(200);      
+    } catch (error) {
+        console.log('something went wrong', error);
+    }
 })
 
 
@@ -46,3 +63,6 @@ mongoose.connect('mongodb+srv://user:1111@cluster0-olmgj.mongodb.net/Pluralsight
 
 
 app.listen(3000);
+
+
+
